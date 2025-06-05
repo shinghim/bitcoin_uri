1c4a1e5850497969adca848b1e131e30a9799927
Shing Him Ng <shinghim@protonmail.com>
2025-06-05 09:32:47 -0500

    Run linter

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //! * Rust-idiomatic: uses strong types, standard traits and other things
 //! * Compliant: implements all requirements of BIP21, including protections to not forget about
-//!              `req-`. (But see features.)
+//!   `req-`. (But see features.)
 //! * Flexible: enables parsing/serializing additional arguments not defined by BIP21
 //! * Performant: uses zero-copy deserialization and lazy evaluation wherever possible.
 //!
